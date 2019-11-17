668fe1be02032251a28b9f4f1800530b082d165d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-17 20:22:59 +0000

    Deploy Chaffity/streamfiles to github.com/Chaffity/streamfiles.git:master

---
+++ main.6229eda7.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Qnk5":[function(require,module,exports) {
+"use strict";module.exports=(({build:e,append:t},s=(()=>{}))=>{const d=e=>(n=>Object.assign(e,{...s(n),add:e=>n((s,d)=>t(d,e(s))),lift:e=>n((s,d)=>t(d,e(s)(s))),scope:t=>d(s=>e(t(s)))}))(t=>d(s=>(t(s,s=e(s)),s)));return new Proxy(d,{get:(t,s)=>d(()=>e(s))})});
+},{}],"x30l":[function(require,module,exports) {
+"use strict";const e=require("@emphori/fui-core");module.exports=e({build:e=>document.createElement(e),append(e,t){e.appendChild(t)}},e=>({text:t=>e((e,r)=>r.innerHTML=t),attr:(t,r)=>e((e,i)=>i.setAttribute(t,r)),prop:(t,r)=>e((e,i)=>i[t]=r)}));
+},{"@emphori/fui-core":"Qnk5"}],"WwlF":[function(require,module,exports) {
+"use strict";module.exports=require("@emphori/fui-html");
+},{"@emphori/fui-html":"x30l"}],"Uylb":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("@emphori/fui/html");document.querySelector("body").appendChild(e.main.text("hello")());
+},{"@emphori/fui/html":"WwlF"}]},{},["Uylb"], null)
+//# sourceMappingURL=main.6229eda7.js.map
